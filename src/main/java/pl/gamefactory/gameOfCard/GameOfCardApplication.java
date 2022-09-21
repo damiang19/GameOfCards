5212668a49a5ca92a78653bcb2e9c4885ac8bd9a
damiang19 <81911555+damiang19@users.noreply.github.com>
2022-09-21 21:55:24 +0200

    Add files via upload

---
+++ src/main/java/pl/gamefactory/gameOfCard/GameOfCardApplication.java
@@ -0,0 +1,13 @@
+package pl.gamefactory.gameOfCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOfCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameOfCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/gamefactory/gameOfCard/GameOfCardApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gamefactory.gameOfCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOfCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
