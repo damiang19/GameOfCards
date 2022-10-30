ab2dd115d94a209170d9afb71b66cafbd4b2febb
Damian <damiang19@gmail.com>
2022-10-30 12:49:49 +0100

    Base configuration of websockets and cors

---
+++ src/main/java/pl/gamefactory/gameOfCard/configuration/WebSocketConfiguration.java
@@ -0,0 +1,34 @@
+package pl.gamefactory.gameOfCard.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.Optional;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry
+                                               registry) {
+        CorsConfiguration configuration = new CorsConfiguration();
+        String[] allowedOrigins = Optional.ofNullable
+                (configuration.getAllowedOrigins())
+                .map(origins -> origins.toArray(new String[0]))
+                .orElse(new String[0]);
+
+        registry.addEndpoint("/websocket/yol").setAllowedOrigins("http://localhost:4200")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config){
+        config.enableSimpleBroker("/topic");
+    }
+}
