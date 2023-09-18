b9d5e2beaf4ceb78879789e8e19852b3536b046d
Damian <damiang19@gmail.com>
2023-09-18 20:19:54 +0200

    #4  Refactor - remove unused entities and connected with them classes

--- src/main/java/pl/gamefactory/gameOfCard/controller/DeckController.java
+++ src/main/java/pl/gamefactory/gameOfCard/controller/DeckController.java
@@ -1,9 +1,8 @@
-package pl.gamefactory.gameOfCard.resources;
+package pl.gamefactory.gameOfCard.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.bind.annotation.*;
 import pl.gamefactory.gameOfCard.entities.Deck;
 import pl.gamefactory.gameOfCard.entities.enums.Cards;
@@ -12,7 +11,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api")
