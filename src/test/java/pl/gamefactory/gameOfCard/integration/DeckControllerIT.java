fec46a391704837ac61a970af37497d2a6942906
Damian <damiang19@gmail.com>
2023-11-14 20:47:21 +0100

    #7 Add config for integration tests and write first

--- src/test/java/pl/gamefactory/gameOfCard/integration/DeckControllerIT.java
+++ src/test/java/pl/gamefactory/gameOfCard/integration/DeckControllerIT.java
@@ -1,8 +1,28 @@
 package pl.gamefactory.gameOfCard.integration;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import pl.gamefactory.gameOfCard.GameOfCardApplication;
+import pl.gamefactory.gameOfCard.controller.DeckController;
 
-@DataMongoTest
-
+@ActiveProfiles("test")
+@AutoConfigureDataMongo
+@WebFluxTest(controllers = DeckController.class)
 public class DeckControllerIT {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    void generateDeck() {
+        webTestClient.post().uri("/api/deck/create-new").exchange().expectStatus().isCreated();
+    }
 }
